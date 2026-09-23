a7f8adc39ae3039ba554d094a6238a758454a784
RentHelper <renthelper@example.com>
2026-09-23 23:42:03 +0530

    security: remove sensitive files from git tracking

--- git-commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=%LOCALAPPDATA%\Programs\Git\bin;%PATH%
-cd /d d:\software\RentHelper
-git config user.email "renthelper@example.com"
-git config user.name "RentHelper"
-git commit -m "initial-commit"
